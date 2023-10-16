83fd069a21211ed01dae91175ffc32f4095a9956
Usama <2021se41@student.uet.edu.pk>
2023-10-17 01:45:31 +0500

    OOP Practice

--- Project6.java
+++ Project6.java
@@ -1,5 +1,26 @@
+
+
+class Employee{
+        String name;
+        String Address;
+        int age;
+        void Print()
+        {
+            System.out.println(this.Address);
+            System.out.println(this.age);
+            System.out.println(this.name);
+        }
+        
+}
 public class Project6 {
     public static void main(String[] args) {
+
+        Employee e=new Employee();
+        e.Address="Khanewal";
+        e.age=12;
+        e.name="Usama";
+        e.Print();
+
         
     }
 }
