0fdc3edb587c2734f7519a8b45da530aa8dcc895
usamajutt786 <usamanasir786999@gmail.com>
2023-10-24 21:39:57 +0500

    Ended

--- se41.java
+++ se41.java
@@ -1,39 +1,49 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-class TheaterManagement {
+// Class1: Theater
+class Theater {
     private String theaterName;
     private String city;
     private int noOfScreens;
-    private List<Movie> movieList = new ArrayList<>();
-    private List<Hall> halls = new ArrayList<>();
+    private List<Movie> movies = new ArrayList<>();
+    private List<Hall> halls = new ArrayList();
+    private List<Employee> employees = new ArrayList();
+    private List<Shops> shops = new ArrayList();
+    private List<RestRoom> restrooms = new ArrayList();
     private OperatorRoom operatorRoom;
-    private TicketCounter ticketCounter;
     private ParkingSlots parkingSlots;
+    private TicketCounter ticketCounter;
 
-    public TheaterManagement(String theaterName, String city, int noOfScreens) {
+    public Theater(String theaterName, String city, int noOfScreens) {
         this.theaterName = theaterName;
         this.city = city;
         this.noOfScreens = noOfScreens;
     }
 
     public boolean isOpen() {
-        // Implement logic to check if the theater is open
         return true;
     }
 
-    public void displayMovieNames() {
-        for (Movie movie : movieList) {
-            System.out.println(movie.getMovieName());
-        }
-    }
-
     public void addMovie(Movie movie) {
-        movieList.add(movie);
+        movies.add(movie);
     }
 
     public void addHall(Hall hall) {
         halls.add(hall);
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void addShop(Shops shop) {
+        shops.add(shop);
+    }
+
+    public void addRestRoom(RestRoom restroom) {
+        restrooms.add(restroom);
     }
 
     public void setOperatorRoom(OperatorRoom operatorRoom) {
@@ -47,14 +57,21 @@ class TheaterManagement {
     public void setTicketCounter(TicketCounter ticketCounter) {
         this.ticketCounter = ticketCounter;
     }
+
+    public void displayMovieNames() {
+        for (Movie movie : movies) {
+            System.out.println(movie.getMovieName());
+        }
+    }
+
 }
 
+// Class2: Movie
 class Movie {
     private int movieId;
     private String movieName;
     private String genre;
     private int duration;
-    private Hall hall;
 
     public Movie(int movieId, String movieName, String genre, int duration) {
         this.movieId = movieId;
@@ -74,6 +91,7 @@ class Movie {
     }
 }
 
+// Class3: Hall
 class Hall {
     private int hallId;
     private String hallName;
@@ -96,6 +114,7 @@ class Hall {
     }
 }
 
+// Class4: OperatorRoom
 class OperatorRoom {
     private int interchargeId;
     private String interchargeName;
@@ -108,10 +127,10 @@ class OperatorRoom {
     }
 
     public void control() {
-        // Implement operator room control logic
     }
 }
 
+// Class5: ParkingSlots
 class ParkingSlots {
     private int totalSlots;
     private int slotsOccupied;
@@ -122,14 +141,13 @@ class ParkingSlots {
     }
 
     public void park() {
-        // Implement parking logic
     }
 
     public void pay() {
-        // Implement payment logic
     }
 }
 
+// Class6: Seats
 class Seats {
     private int seatId;
     private int hallId;
@@ -145,10 +163,10 @@ class Seats {
     }
 
     public void displayAvailableSeats() {
-        // Implement logic to display available seats
     }
 }
 
+// Class7: TicketCounter
 class TicketCounter {
     private int ticketId;
     private int customerId;
@@ -165,14 +183,14 @@ class TicketCounter {
     }
 
     public void showTicket() {
-        // Implement logic to display tickets
     }
 
     public void bookTicket() {
-        // Implement ticket booking logic
+
     }
 }
 
+// Class8: Employee
 class Employee {
     private int employeeId;
     private String employeeName;
@@ -187,22 +205,10 @@ class Employee {
     }
 
     public void employeeDetails() {
-        // Implement employee details logic
     }
 }
 
-class SupportStaff extends Employee {
-    public SupportStaff(int employeeId, String employeeName, String designation, double salary) {
-        super(employeeId, employeeName, designation, salary);
-    }
-}
-
-class TechnicalOperator extends Employee {
-    public TechnicalOperator(int employeeId, String employeeName, String designation, double salary) {
-        super(employeeId, employeeName, designation, salary);
-    }
-}
-
+// Class9: Shops
 class Shops {
     private int shopId;
     private int employeeId;
@@ -217,18 +223,19 @@ class Shops {
     }
 
     public void shopDetails() {
-        // Implement shop details logic
+        // Implement shopDetails logic
     }
 
     public void itemDetails() {
-        // Implement item details logic
+        // Implement itemDetails logic
     }
 
     public void buy() {
-        // Implement shopping logic
+        // Implement buy logic
     }
 }
 
+// Class10: Customer
 class Customer {
     private int customerId;
     private String customerName;
@@ -243,10 +250,11 @@ class Customer {
     }
 
     public void customerDetails() {
-        // Implement customer details logic
+        // Implement customerDetails logic
     }
 }
 
+// Class11: RestRoom
 class RestRoom {
     private int noOfStaff;
 
@@ -255,23 +263,80 @@ class RestRoom {
     }
 
     public void use() {
-        // Implement restroom usage logic
+        // Implement use logic
     }
 }
 
 public class se41 {
     public static void main(String[] args) {
-        // Create instances of the classes and implement the main logic for the theater management system
-        TheaterManagement theater = new TheaterManagement("ABC Theater", "City", 5);
-        Movie movie1 = new Movie(1, "Movie A", "Action", 120);
-        Movie movie2 = new Movie(2, "Movie B", "Comedy", 90);
-        Hall hall1 = new Hall(1, "Hall 1", 1);
-        Seats seats1 = new Seats(1, 1, "Regular", 100);
-        hall1.setSeats(seats1);
-        theater.addMovie(movie1);
-        theater.addMovie(movie2);
-        theater.addHall(hall1);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Theater Management System!");
+        int choice;
 
-        // Implement more logic as per your requirements
+        Theater theater = null;
+        do {
+            System.out.println("1. Add Theater");
+            System.out.println("2. Add Movie");
+            System.out.println("3. Show Theater");
+            System.out.println("4. Show Movie");
+            System.out.println("Please enter your choice (-1 to exit):");
+            choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Enter theater name:");
+                String theaterName = scanner.next();
+                System.out.println("Enter city:");
+                String city = scanner.next();
+                System.out.println("Enter the number of screens:");
+                int noOfScreens = scanner.nextInt();
+                theater = new Theater(theaterName, city, noOfScreens);
+            } else if (choice == 2) {
+                System.out.println("Enter movie details:");
+                System.out.println("Enter movie ID:");
+                int movieId = scanner.nextInt();
+
+                System.out.println("Enter movie name:");
+                scanner.nextLine(); // Consume newline
+                String movieName = scanner.nextLine();
+
+                System.out.println("Enter genre:");
+                String genre = scanner.nextLine();
+                System.out.println("Enter duration (in minutes):");
+                int duration = scanner.nextInt();
+
+                Movie movie = new Movie(movieId, movieName, genre, duration);
+                theater.addMovie(movie);
+                System.out.println("You selected choice 2.");
+
+            } else if (choice == 3) {
+                theater.displayMovieNames();
+            } else if (choice == 4) {
+
+            } else if (choice == -1) {
+                System.out.println("Exiting the program.");
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != -1);
+
+        boolean continueAddingMovies = true;
+
+        while (continueAddingMovies) {
+            System.out.println("Do you want to add another movie? (yes/no)");
+            String addAnotherMovie = scanner.next();
+            if (!addAnotherMovie.equalsIgnoreCase("yes")) {
+                continueAddingMovies = false;
+            }
+        }
+
+        if (theater != null) {
+            System.out.println("Movie Names:");
+            theater.displayMovieNames();
+        }
+
+        // Implement similar logic for other objects like Hall, OperatorRoom,
+        // ParkingSlots, TicketCounter, etc.
+
+        // Close the scanner
+        scanner.close();
     }
 }
